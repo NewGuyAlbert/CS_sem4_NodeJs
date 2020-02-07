77fccb6cdf35d560433b5a3830a7dcce5d258610
NewGuyAlbert <albe9220@stud.kea.dk>
2020-02-07 12:56:24 +0100

    Json

---
+++ Lesson_1_intro_files/1_exercices/2_Strings.js
@@ -0,0 +1,45 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch neither line 1 or line 2
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+var one = 10;
+var two = 45;
+var three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+// --------------------------------------
+// console.log((parseFloat(numberOne) + parseFloat(numberTwo)).toFixed(2))
+// console.log(((one + two + three) / 3).toFixed(5))
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+var letters = "abc"
+// Get me the character "c"
+
+console.log(letters.indexOf('c',0))
+// --------------------------------------
+// Exercise 7 - Replace
+
+var fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+console.log(fact.replace('j', 'J'))
+
+// --------------------------------------
--- Lesson_1_intro_files/FirstFile.js
+++ Lesson_1_intro_files/FirstFile.js
@@ -1,5 +1,7 @@
-var name = "Jakub";
-var age = 123;
+// var name = "Jakub";
+// var age = 123;
 
-console.log(name, "is",age,"years old");
+// console.log(name, "is",age,"years old");
+
+var computer = {};
 
